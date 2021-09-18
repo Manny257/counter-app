ef39e5ded3fdf509423da9558250f0e3bbb056d7
Manny <mannygeo25@gmail.com>
2021-09-19 00:15:54 +0200

    before composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,22 +3,25 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
   };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
-        <button className="btn btn-primary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}> {tag} </li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-primary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 bg-";
